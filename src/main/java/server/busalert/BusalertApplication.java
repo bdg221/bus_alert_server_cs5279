940c4c7e48570f1cbbd888aa5b036bead9621725
bdg221 <brian.d.goldsmith@vanderbilt.edu>
2022-06-13 14:41:48 -0700

    Initial commit from Spring Boot Initializer

---
+++ src/main/java/server/busalert/BusalertApplication.java
@@ -0,0 +1,13 @@
+package server.busalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/busalert/BusalertApplicationTests.java
@@ -0,0 +1,13 @@
+package server.busalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
