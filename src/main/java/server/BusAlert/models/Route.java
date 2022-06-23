53a1267ef1b9ab5c931241997a2c7c8ae0c89c93
bdg221 <brian.d.goldsmith@vanderbilt.edu>
2022-06-23 07:20:56 -0700

    update constructor and getter/setter for #3

--- src/main/java/server/BusAlert/models/Route.java
+++ src/main/java/server/BusAlert/models/Route.java
@@ -1,6 +1,7 @@
 package server.BusAlert.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -12,7 +13,37 @@ public class Route {
 
     private String code;
 
-    @OneToMany
-    private List<Stop> stops;
+    @OneToMany(mappedBy = "route")
+    private List<Stop> stops = new ArrayList<>();
 
+    public Route() {
+    }
+
+    public Route(String code) {
+        this.code = code;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Stop> getStops() {
+        return stops;
+    }
+
+    public void setStops(List<Stop> stops) {
+        this.stops = stops;
+    }
 }
