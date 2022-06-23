1e13a6abcf7eae9084461daed3383bd749b6257c
bdg221 <brian.d.goldsmith@vanderbilt.edu>
2022-06-23 07:19:22 -0700

    update constructor and getter/setter for #5

--- src/main/java/server/BusAlert/models/Rider.java
+++ src/main/java/server/BusAlert/models/Rider.java
@@ -11,5 +11,29 @@ public class Rider {
     private String phone;
 
     @ManyToOne
+    @JoinColumn(name = "stop_id")
     private Stop stop;
+
+    public Rider() {
+    }
+
+    public Rider(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Stop getStop() {
+        return stop;
+    }
+
+    public void setStop(Stop stop) {
+        this.stop = stop;
+    }
 }
