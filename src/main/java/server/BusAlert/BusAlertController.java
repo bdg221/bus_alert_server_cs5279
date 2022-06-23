c8c4e58acc89f1b181ed9c30c96a07b62a724e1d
bdg221 <brian.d.goldsmith@vanderbilt.edu>
2022-06-23 07:40:23 -0700

    build addRoute Close #9

--- src/main/java/server/BusAlert/BusAlertController.java
+++ src/main/java/server/BusAlert/BusAlertController.java
@@ -1,7 +1,47 @@
 package server.BusAlert;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import server.BusAlert.models.Route;
 
 @RestController
 public class BusAlertController {
+
+    public static final String API_VERSION = "1.0";
+
+    public static final String RIDER_PATH = "/api/" + API_VERSION +"/rider";
+    public static final String STOP_PATH = "/api/" + API_VERSION +"/stop";
+    public static final String ROUTE_PATH = "/api/" + API_VERSION +"/route";
+
+    public static final String GPS_PATH = "/api/" + API_VERSION +"/gps";
+
+    @Autowired
+    private BusAlertService busAlertService;
+
+
+    /**
+     * The method uses takes an HTTP POST request containing
+     *
+     * @param routeId - this is the
+     * @param latitude
+     * @param longitude
+     */
+    @PostMapping(GPS_PATH)
+    public void receiveGPS(
+            @RequestParam("routeId") String routeId,
+            @RequestParam("latitude") String latitude,
+            @RequestParam("longitude") String longitude
+    ){
+        // pass the info to the BusAlertService to handle the business logic
+    }
+
+    @PostMapping(ROUTE_PATH)
+    public Route addRoute(
+            @RequestParam("code") String code
+    ){
+        return busAlertService.addRoute(code);
+    }
+
 }
--- src/main/java/server/BusAlert/BusAlertService.java
+++ src/main/java/server/BusAlert/BusAlertService.java
@@ -1,7 +1,19 @@
 package server.BusAlert;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import server.BusAlert.models.Route;
+import server.BusAlert.repositories.RouteRepository;
 
 @Service
 public class BusAlertService {
+
+    @Autowired
+    private RouteRepository routeRepository;
+
+    public Route addRoute(String code){
+        Route route = new Route(code);
+        return routeRepository.save(route);
+    }
+
 }
