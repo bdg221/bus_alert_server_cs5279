5d8dd6b9bc1df14f89405a4bba977f1007587547
bdg221 <brian.d.goldsmith@vanderbilt.edu>
2022-06-23 07:06:29 -0700

    create Route Model for #3

---
+++ src/main/java/server/BusAlert/models/Route.java
@@ -0,0 +1,19 @@
+package server.BusAlert.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Route {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String code;
+
+    @OneToMany
+    private List<Stop>;
+
+
+}
