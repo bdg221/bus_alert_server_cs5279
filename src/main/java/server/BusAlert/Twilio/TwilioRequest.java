52283a77a1bb00fc08ba163f08b73799e80ec589
Brian G <62308289+bdg221@users.noreply.github.com>
2022-07-15 02:25:41 -0700

    add getters for message and phoneNumber
    I know I am going to need this when calling TwilioService.SendMessage

--- src/main/java/server/BusAlert/Twilio/TwilioRequest.java
+++ src/main/java/server/BusAlert/Twilio/TwilioRequest.java
@@ -3,4 +3,8 @@ package server.BusAlert.Twilio;
 public class TwilioRequest {
     public String phoneNumber;
     public String message;
+    
+    public String getMessage(){ return this.message; }
+    public String getPhoneNumber(){ return this.phoneNumber; }
+    
 }
