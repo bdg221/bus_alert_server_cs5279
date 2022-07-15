9398917f1c5a42b3a2d66b51a1b101d4d7e8260a
Brian G <62308289+bdg221@users.noreply.github.com>
2022-07-15 02:22:22 -0700

    utilize Spring's Service annotation
    just added a few Spring specific stuff

--- src/main/java/server/BusAlert/Twilio/TwilioService.java
+++ src/main/java/server/BusAlert/Twilio/TwilioService.java
@@ -1,5 +1,8 @@
 package server.BusAlert.Twilio;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TwilioService {
     public static boolean SendMessage(TwilioRequest request) {
         System.out.println("Sending \"" + request.message + "\" to " + request.phoneNumber);
